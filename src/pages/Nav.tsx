e5d1e7ac9be07cff9e2f8f5327b9b432d39734c0
Gabriel Ivanes <ivanes.gabriel@gmail.com>
2023-10-13 18:47:27 +0200

    fix nav

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -10,21 +10,6 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-import image1 from '../assets/1.png';
-import image2 from '../assets/2.png';
-import image3 from '../assets/3.png';
-import image4 from '../assets/4.png';
-import image5 from '../assets/5.png';
-import image6 from '../assets/6.png';
-import image7 from '../assets/7.png';
-import image8 from '../assets/8.png';
-import image9 from '../assets/9.png';
-import image10 from '../assets/10.png';
-import image11 from '../assets/11.png';
-import image12 from '../assets/12.png';
-import image13 from '../assets/13.png';
-import image14 from '../assets/14.png';
-import image15 from '../assets/15.png';
 
 interface Props {
 	userId: string;
@@ -149,11 +134,6 @@ function Nav({ userId, baseUrlBack, xsrfToken }: Props) {
 						<FontAwesomeIcon icon={faArrowRightFromBracket} className='icon' />
 					</div>
 				)}
-			</div>
-			<div style={{ visibility: 'hidden' }}>
-				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((index) => (
-					<img src={`image${index}`} alt='nav' />
-				))}
 			</div>
 		</div>
 	);
